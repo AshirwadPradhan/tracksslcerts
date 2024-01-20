32b0129d0e4f6296ac5fbf1f4033ab880eec12a2
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-01-20 18:18:18 +0530

    removed commented code

--- types/user.go
+++ types/user.go
@@ -14,21 +14,3 @@ func NewUser(userName string, email string, password string) *User {
 		HashedPassword: password,
 	}
 }
-
-// func (u *User) AddDomainToTrack(domain string) {
-// 	sci := NewSSLCertInfo(domain)
-// 	sci.Validate()
-// 	u.TrackedDomains = append(u.TrackedDomains, *sci)
-// }
-
-// func (u *User) RemoveTrackedDomain(domain string) {
-// 	idx := 0
-// 	for i, d := range u.TrackedDomains {
-// 		if d.Domain == domain {
-// 			idx = i
-// 			break
-// 		}
-// 	}
-// 	before := u.TrackedDomains[:idx]
-// 	u.TrackedDomains = append(before, u.TrackedDomains[idx+1:]...)
-// }
